082d5df4a5106569dec132253341bc308e6f6757
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2025-05-06 13:32:13 -0700

    speech input working

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,9 +2,19 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
+const inputTxt = document.querySelector("#text-to-speak");
+const voiceSelect = document.getElementById("voice-select");
+
+let voices = [];
+
 function init() {
   // TODO
   populateVoiceList();
+
+  const speech_button = document.querySelector("#explore button");
+  speech_button.addEventListener("click", () => {
+    speak();
+  });
 }
 
 function populateVoiceList() {
@@ -12,7 +22,8 @@ function populateVoiceList() {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-  const voices = speechSynthesis.getVoices();
+  voices = speechSynthesis.getVoices();
+  voiceSelect.innerHTML = "";
 
   for (let i = 0; i < voices.length; i++) {
     const option = document.createElement("option");
@@ -25,5 +36,32 @@ function populateVoiceList() {
     option.setAttribute("data-lang", voices[i].lang);
     option.setAttribute("data-name", voices[i].name);
     document.getElementById("voice-select").appendChild(option);
+  }
+}
+
+function speak() {
+  if (speechSynthesis.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+
+  if (inputTxt.value !== "") {
+    if (!voiceSelect.selectedOptions.length) {
+      console.warn("No voice selected.");
+      return;
+    }
+
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    speechSynthesis.speak(utterThis);
   }
 }
