07e8e1cfc9d8b4b6bbc0b6f312edab36aaea2a6e
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2025-05-06 13:08:29 -0700

    populate list of voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,29 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   // TODO
+  populateVoiceList();
+}
+
+function populateVoiceList() {
+  if (typeof speechSynthesis !== "undefined") {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
 }
