f8ec594de881980454caf673f16e6a630c41e5b4
kevincsware <kevin@swareco.com>
2025-05-05 21:16:23 -0700

    horn dropdown sets corresponding image and audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,16 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const image = document.querySelector('img[src="assets/images/no-image.png"]');
+  const audio = document.querySelector('#expose audio');
+  const horn_btn = document.getElementById("horn-select");
+
+  // horn dropdown sets corresponding image/audio
+  horn_btn.addEventListener('change', (event) => {
+    const choice = event.target.value;  
+    image.src = `assets/images/${choice}.svg`;
+    image.alt = choice.replace('-', ' ');
+    audio.src = `assets/audio/${choice}.mp3`;
+  });
+
 }
